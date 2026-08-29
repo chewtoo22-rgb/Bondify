c11b070ec45bb6eb6d8ecd715498f99b45af076a
Matt H <chewtoo22@gmail.com>
2026-08-29 05:30:53 -0400

    enrollment: normalize whitespace before rejecting controls

--- core/enrollment/enrollment.go
+++ core/enrollment/enrollment.go
@@ -77,15 +77,15 @@ func NormalizeDeviceName(name string) (string, error) {
 	b.Grow(len(name))
 	lastSpace := false
 	for _, r := range name {
-		if unicode.IsControl(r) {
-			return "", ErrDeviceName
-		}
 		if unicode.IsSpace(r) {
 			if !lastSpace {
 				b.WriteByte(' ')
 				lastSpace = true
 			}
 			continue
+		}
+		if unicode.IsControl(r) {
+			return "", ErrDeviceName
 		}
 		b.WriteRune(r)
 		lastSpace = false
